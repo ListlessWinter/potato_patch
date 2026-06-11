bf0af9a964e6310154ac9ce91b7d5dfeecca1980
Izumi <131525503+ListlessWinter@users.noreply.github.com>
2026-06-11 10:46:36 +0800

    Fix dynamic viewport zooming by using proportional scaling instead of hardcoding, preserving fixed offset for UI

--- main.c
+++ main.c
@@ -713,23 +713,25 @@ void patchGeList(u32 *list, u32 *stall) {
 
       case GE_CMD_VIEWPORTXSCALE:
         t.i = data << 8;
-        t.f = (t.f < 0) ? -(WIDTH / 2) : (WIDTH / 2);
+        t.f *= 2.0f;
         *list = (cmd << 24) | (t.i >> 8);
         break;
 
       case GE_CMD_VIEWPORTYSCALE:
         t.i = data << 8;
-        t.f = (t.f < 0) ? -(HEIGHT / 2) : (HEIGHT / 2);
+        t.f *= 2.0f;
         *list = (cmd << 24) | (t.i >> 8);
         break;
 
       case GE_CMD_VIEWPORTXCENTER:
-        t.f = 2048;
+        t.i = data << 8;
+        t.f = 2048.0f + (t.f - 2048.0f) * 2.0f;
         *list = (cmd << 24) | (t.i >> 8);
         break;
 
       case GE_CMD_VIEWPORTYCENTER:
-        t.f = 2048;
+        t.i = data << 8;
+        t.f = 2048.0f + (t.f - 2048.0f) * 2.0f;
         *list = (cmd << 24) | (t.i >> 8);
         break;
 
